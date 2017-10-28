2edbb49bba2e7bbeb142373160e24e766dadc1c9
pl5736 <liup5736@gmail.com>
2017-10-28 09:19:25 +0800

    something update

--- pl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get install build-essential cmake
-sudo apt-get install build-essential cmake3
-sudo apt-get install python-dev python3-dev
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
-rm -rf ~/.pl5736vim
--- pl5736vim.sh
+++ pl5736vim.sh
@@ -5,3 +5,9 @@ mv ~/pl5736vim/.vimrc.bundles ~
 mv ~/pl5736vim/.vimrc.bundles.local ~
 mv ~/pl5736vim/.vimrc.local ~
 mv ~/pl5736vim/.vim ~
+sudo apt-get install build-essential cmake
+sudo apt-get install build-essential cmake3
+sudo apt-get install python-dev python3-dev
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
+rm -rf ~/.pl5736vim
