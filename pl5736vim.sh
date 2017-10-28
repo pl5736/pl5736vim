08230fbe1f02eb1e3e9262ee47f6fa3b02c30b81
pl5736 <31681207+pl5736@users.noreply.github.com>
2017-10-28 10:34:47 +0800

    Add files via upload

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-mv ~/pl5736vim/.vimrc ~ -b -S _old
-mv ~/pl5736vim/.vimrc.before ~ -b -S _old
-mv ~/pl5736vim/.vimrc.bundles ~ -b -S _old
-mv ~/pl5736vim/.vimrc.bundles.local ~ -b -S _old
-mv ~/pl5736vim/.vimrc.local ~ -b -S _old
-mv ~/pl5736vim/.vim ~ -b -S _old
+mv ~/pl5736vim/.vimrc ~
+mv ~/pl5736vim/.vimrc.before ~
+mv ~/pl5736vim/.vimrc.bundles ~
+mv ~/pl5736vim/.vimrc.bundles.local ~
+mv ~/pl5736vim/.vimrc.local ~
+mv ~/pl5736vim/.vim ~
+sudo apt-get install build-essential cmake
+sudo apt-get install build-essential cmake3
+sudo apt-get install python-dev python3-dev
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
 rm -rf ~/pl5736vim
