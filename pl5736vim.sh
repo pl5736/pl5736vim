3fd572b859c81380aef0ec621aa6b49b444bddee
pl5736 <31681207+pl5736@users.noreply.github.com>
2017-10-28 10:19:37 +0800

    Update pl5736vim.sh

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -5,9 +5,4 @@ mv ~/pl5736vim/.vimrc.bundles ~ -b -S _old
 mv ~/pl5736vim/.vimrc.bundles.local ~ -b -S _old
 mv ~/pl5736vim/.vimrc.local ~ -b -S _old
 mv ~/pl5736vim/.vim ~ -b -S _old
-sudo apt-get install build-essential cmake
-sudo apt-get install build-essential cmake3
-sudo apt-get install python-dev python3-dev
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
 rm -rf ~/.pl5736vim
