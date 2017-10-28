3dcbffe009bdaa6b7b5dcd61a1be25d6629c8d34
pl5736 <31681207+pl5736@users.noreply.github.com>
2017-10-28 10:20:47 +0800

    Update pl5736vim.sh

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -5,4 +5,4 @@ mv ~/pl5736vim/.vimrc.bundles ~ -b -S _old
 mv ~/pl5736vim/.vimrc.bundles.local ~ -b -S _old
 mv ~/pl5736vim/.vimrc.local ~ -b -S _old
 mv ~/pl5736vim/.vim ~ -b -S _old
-rm -rf ~/.pl5736vim
+rm -rf ~/pl5736vim
