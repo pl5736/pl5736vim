ccb1e0a2907b0e1274e81c04764b24807a9c1dac
pl5736 <31681207+pl5736@users.noreply.github.com>
2017-10-30 10:20:07 +0800

    Update pl5736vim.sh

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-mv ~/pl5736vim/.vimrc ~ -b -S _old
-mv ~/pl5736vim/.vimrc.before ~ -b -S _old
-mv ~/pl5736vim/.vimrc.bundles ~ -b -S _old
-mv ~/pl5736vim/.vimrc.bundles.local ~ -b -S _old
-mv ~/pl5736vim/.vimrc.local ~ -b -S _old
-mv ~/pl5736vim/.vim ~ -b -S _old
+cp ~/pl5736vim/.vimrc ~ -b -S _old
+cp ~/pl5736vim/.vimrc.before ~ -b -S _old
+cp ~/pl5736vim/.vimrc.bundles ~ -b -S _old
+cp ~/pl5736vim/.vimrc.bundles.local ~ -b -S _old
+cp ~/pl5736vim/.vimrc.local ~ -b -S _old
+cp ~/pl5736vim/.vim ~ -b -S _old
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouComPleteMe
 ./install.py --clang-completer
 rm -rf ~/.vim/bundle/vundle
 mv ~/.vim/bundle/Vundle.vim ~/.vim/bundle/vundle
-rm -rf ~/pl5736vim
