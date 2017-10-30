925fabaf1a3c3a9751d975596247129d344e6a63
pl5736 <31681207+pl5736@users.noreply.github.com>
2017-10-30 15:45:41 +0800

    Update pl5736vim.sh

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo apt-get -y install vim
+sudo apt-get -y install build-essential cmake
+sudo apt-get -y install python-dev python3-dev
+sudo apt-get -y install build-essential cmake3
 cp ~/pl5736vim/.vimrc ~ -b -S _old
 cp ~/pl5736vim/.vimrc.before ~ -b -S _old
 cp ~/pl5736vim/.vimrc.bundles ~ -b -S _old
