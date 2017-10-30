8fa371d16a35df9d985ecffd9849eb09f3a28249
pl5736 <31681207+pl5736@users.noreply.github.com>
2017-10-30 10:48:21 +0800

    Update pl5736vim.sh

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -4,7 +4,8 @@ cp ~/pl5736vim/.vimrc.before ~ -b -S _old
 cp ~/pl5736vim/.vimrc.bundles ~ -b -S _old
 cp ~/pl5736vim/.vimrc.bundles.local ~ -b -S _old
 cp ~/pl5736vim/.vimrc.local ~ -b -S _old
-cp ~/pl5736vim/.vim ~ -b -S _old
+mv ~/.vim ~/.vim_old
+cp ~/pl5736vim/.vim ~ -r
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouComPleteMe
 ./install.py --clang-completer
