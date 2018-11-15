68a9b70da74e224e5fa40ccd5f1c489a7b4bb065
pl5736 <31681207+pl5736@users.noreply.github.com>
2018-11-15 23:40:17 +0800

    Update pl5736vim.sh

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -4,7 +4,7 @@ sudo apt-get -y install git
 sudo apt-get -y install build-essential cmake
 sudo apt-get -y install build-essential cmake3
 sudo apt-get -y install python-dev python3-dev
-sudo pip3 install autopep8
+sudo pip install autopep8
 git clone https://github.com/pl5736/pl5736vim.git ~/pl5736vim
 cp ~/pl5736vim/.vimrc ~ -b -S _old
 cp ~/pl5736vim/.vimrc.before ~ -b -S _old
