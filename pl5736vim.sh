cec3180063688bcefb39729319395a5d0450c291
pl5736 <31681207+pl5736@users.noreply.github.com>
2023-11-14 18:59:41 +0800

    Update pl5736vim.sh
    Update pl5736vim.sh

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-sudo apt-get -y install vim
-sudo apt-get -y install git
-sudo apt-get -y install vim-gtk
-sudo apt-get -y install build-essential cmake vim-nox python3-dev
-sudo apt-get -y install mono-complete golang nodejs default-jdk npm
-sudo apt-get -y install python-pip
+sudo apt -y install vim
+sudo apt -y install git
+sudo apt -y install vim-gtk
+sudo apt -y install build-essential cmake vim-nox python3-dev
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt -y install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
+sudo apt -y install python-pip
 sudo pip install autopep8
 git clone https://github.com/pl5736/pl5736vim.git ~/pl5736vim
 cp ~/pl5736vim/.vimrc ~ -b -S _old
@@ -12,6 +15,8 @@ cp ~/pl5736vim/.vimrc.before ~ -b -S _old
 cp ~/pl5736vim/.vimrc.bundles ~ -b -S _old
 cp ~/pl5736vim/.vimrc.bundles.local ~ -b -S _old
 cp ~/pl5736vim/.vimrc.local ~ -b -S _old
+cp ~/pl5736vim/.vimrc.idea ~ -b -S _old
+cp ~/pl5736vim/.ideavimrc ~ -b -S _old
 cd ~
 if [ -d ".vim" ]; then
     mv ~/.vim ~/.vim_old
