1f635f163e9efff5f7e2631c137f42f11c0ec197
pl5736 <31681207+pl5736@users.noreply.github.com>
2023-11-14 19:27:42 +0800

    Update pl5736vim.sh
    fix install nodejs

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -6,7 +6,10 @@ sudo apt -y install build-essential cmake vim-nox python3-dev
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
 sudo apt -y install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
+sudo rm -r /etc/apt/sources.list.d/nodesource.list
+sudo rm -r /etc/apt/keyrings/nodesource.gpg
 sudo apt -y install python-pip
 sudo pip install autopep8
 git clone https://github.com/pl5736/pl5736vim.git ~/pl5736vim
