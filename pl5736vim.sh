259acfbeb3b64710348590d0229f9e34fc104281
pl5736 <31681207+pl5736@users.noreply.github.com>
2023-12-04 17:17:58 +0800

    Update pl5736vim.sh
    update: python code pep8

--- pl5736vim.sh
+++ pl5736vim.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sudo apt -y install vim
-sudo apt -y install git
-sudo apt -y install vim-gtk
+sudo apt -y install git vim vim-gtk
 sudo apt -y install build-essential cmake vim-nox python3-dev
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
@@ -10,8 +8,8 @@ sudo apt update
 sudo apt -y install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre
 sudo rm -r /etc/apt/sources.list.d/nodesource.list
 sudo rm -r /etc/apt/keyrings/nodesource.gpg
-sudo apt -y install python-pip
-sudo pip install autopep8
+sudo apt -y install python3-pip
+sudo pip3 install autopep8 flake8
 git clone https://github.com/pl5736/pl5736vim.git ~/pl5736vim
 cp ~/pl5736vim/.vimrc ~ -b -S _old
 cp ~/pl5736vim/.vimrc.before ~ -b -S _old
